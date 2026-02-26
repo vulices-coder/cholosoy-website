3ead3e5e849d6eb501a80021bb59f7ad906857ef
CholoCreator <v.ulices@gmail.com>
2026-02-26 19:22:49 +0100

    Home & Kontakt Page

---
+++ src/app/(marketing)/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <p>Marketing Layout con Navbar + Footer.</p>
+    </main>
+  );
+}
---
+++ src/app/(marketing)/kontakt/page.tsx
@@ -0,0 +1,8 @@
+export default function KontaktPage() {
+  return (
+    <main>
+      <h1>Kontakt / Reservierung</h1>
+      <p>Aquí irá el formulario (Client Component) más adelante.</p>
+    </main>
+  );
+}
