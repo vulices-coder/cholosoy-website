d408d9f57e3f7efb10c57ea16fb498d7b7bb83da
CholoCreator <v.ulices@gmail.com>
2026-03-11 18:40:51 +0100

    refactor(footer): add legal links, brand label and social placeholders

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,22 +1,42 @@
+"use client";
+
 import Link from "next/link";
-import Container from "@/components/ui/Container";
+import { usePathname } from "next/navigation";
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
+  const pathname = usePathname();
+  const locale = pathname.split("/")[1] || "de";
+
   return (
     <footer className={styles.footer}>
-      <Container size="lg">
-        <div className={styles.footerInner}>
-          <nav className={styles.links}>
-            <Link href="/agb">AGB</Link>
-            <Link href="/datenschutz">Datenschutz</Link>
-            <Link href="/kontakt">Kontakt</Link>
-            <Link href="/impressum">Impressum</Link>
-          </nav>
-
-          <div className={styles.meta}>© {new Date().getFullYear()} CholoSoy</div>
+      <div className={styles.footerInner}>
+        
+        {/* LEFT */}
+        <div className={styles.brand}>
+          CholoSoy 2026
         </div>
-      </Container>
+
+        {/* CENTER */}
+        <nav className={styles.legal}>
+          <Link href={`/${locale}/agb`}>AGB</Link>
+          <Link href={`/${locale}/datenschutz`}>Datenschutz</Link>
+          <Link href={`/${locale}/info`}>Info</Link>
+          <Link href={`/${locale}/impressum`}>Impressum</Link>
+        </nav>
+
+        {/* RIGHT */}
+        <div className={styles.social}>
+          <a href="#" aria-label="Facebook">
+            Facebook
+          </a>
+
+          <a href="#" aria-label="Instagram">
+            Instagram
+          </a>
+        </div>
+
+      </div>
     </footer>
   );
 }
